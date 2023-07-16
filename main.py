fb205c9997f2f0c0dd67c4abf8b0c2b05306eca2
Mohamed Samer <mohamed.s.zaina@gmail.com>
2023-07-16 13:39:34 +0300

    code updated After pycodestyle

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 import time
 import random
 
+
 def print_pause(*messages):
     for message in messages:
         print(message)
         time.sleep(2)
 
+
 def intro():
-    print_pause("You find yourself standing in an open field, filled with grass",
-                "and yellow wildflowers.")
-    print_pause("Rumor has it that a wicked fairy is somewhere around here,",
-                "and has been terrifying the nearby village.")
+    print_pause(
+        "You find yourself standing in an open field, filled with grass "
+        "and yellow wildflowers."
+    )
+    print_pause(
+        "Rumor has it that a wicked fairy is somewhere around here, "
+        "and has been terrifying the nearby village."
+    )
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
-    print_pause("In your hand, you hold your trusty (but not very effective)",
-                "dagger.")
+    print_pause(
+        "In your hand, you hold your trusty (but not very effective) "
+        "dagger."
+    )
+
 
 def get_choice():
-    choice = input("What would you like to do?\n"
-                   "1. Knock on the door of the house.\n"
-                   "2. Peer into the cave.\n"
-                   "Please enter 1 or 2.\n")
-    while choice != '1' and choice != '2':
+    choice = input(
+        "What would you like to do?\n"
+        "1. Knock on the door of the house.\n"
+        "2. Peer into the cave.\n"
+        "Please enter 1 or 2.\n"
+    )
+    while choice != "1" and choice != "2":
         choice = input("Please enter 1 or 2.\n")
     return choice
+
 
 def fight(enemy):
     print_pause("You decide to stand your ground and fight!")
@@ -34,18 +46,22 @@ def fight(enemy):
         print_pause(f"Oh no! The {enemy} overpowers you.")
         print_pause("Game over.")
 
+
 def cave():
     print_pause("You peer cautiously into the cave.")
     if random.randint(0, 1) == 0:
         print_pause("You enter the cave and discover a treasure chest!")
-        reward = random.choice(["magic potion", "gold coins", "ancient artifact"])
+        reward = random.choice(["magic potion", "gold coins",
+                                "ancient artifact"])
         print_pause(f"You found a {reward}! Congratulations!")
         print_pause("You win!")
     else:
-        enemy = random.choice(["vicious troll", "ferocious dragon", "cunning goblin"])
+        enemyList = ["vicious troll", "ferocious dragon", "cunning goblin"]
+        enemy = random.choice(enemyList)
         print_pause(f"You enter the cave and encounter a {enemy}.")
         print_pause(f"The {enemy} attacks you!")
         fight(enemy)
+
 
 def field():
     print_pause("You run back to the field.")
@@ -54,38 +70,44 @@ def field():
         print_pause("You stumble upon a hidden path.")
         print_pause("Do you want to explore it?")
         explore_choice = input("Enter 'y' for yes or 'n' for no.\n").lower()
-        if explore_choice == 'y':
+        if explore_choice == "y":
             print_pause("You follow the path and find a secret treasure!")
             print_pause("Congratulations! You win!")
         else:
             print_pause("You decide not to explore the hidden path.")
             print_pause("You continue your adventure.")
 
+
 def house():
     print_pause("You approach the door of the house.")
-    print_pause("You are about to knock when the door opens and out steps",
-                "a wicked fairy.")
+    print_pause(
+        "You are about to knock when the door opens and out steps"
+        " a wicked fairy."
+    )
     print_pause("Eep! This is the wicked fairy's house!")
     print_pause("The wicked fairy attacks you!")
     fight("wicked fairy")
 
+
 def play_game():
     intro()
     choice = get_choice()
-    if choice == '1':
+    if choice == "1":
         house()
     else:
         cave()
     play_again()
 
+
 def play_again():
     choice = input("Would you like to play again? (y/n)\n").lower()
-    while choice != 'y' and choice != 'n':
+    while choice != "y" and choice != "n":
         choice = input("Please enter 'y' or 'n'.\n").lower()
-    if choice == 'y':
+    if choice == "y":
         print_pause("\n\n\nExcellent! Restarting the game ...\n\n\n")
         play_game()
     else:
         print_pause("\n\n\nThanks for playing! Goodbye!")
+
 
 play_game()
